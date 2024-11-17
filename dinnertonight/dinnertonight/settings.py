6fc9c72e4a52adfe60d1262342c98f77267ab1a3
Dakshith L <dakzsh24@gmail.com>
2024-11-17 13:33:43 +0530

    Update render deploy link

--- dinnertonight/dinnertonight/settings.py
+++ dinnertonight/dinnertonight/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'your-app-name.onrender.com',
+    'dinnertonight-t0kl.onrender.com',
     'localhost',
     '127.0.0.1'
 ]
